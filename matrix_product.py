936a1464b300e5b7cb440277b8cde38d45c0c2a1
Bastian Spatz <spatz.bastian@mnet-mail.de>
2025-06-12 15:44:26 +0200

    Add matrix_product.py

---
+++ matrix_product.py
@@ -0,0 +1,9 @@
+def matrix_multiply(A, B):
+    if len(A[0]) != len(B):
+        raise ValueError("Incompatible dimensions for matrix multiplication.")
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
